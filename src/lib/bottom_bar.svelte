43727b13ac0201f99dda4bfe4d0f9e89d9485e9e
Nils Herzig <git@nilsherzig.com>
2024-03-30 17:58:09 +0100

    ui: chatbox no longer same with as answers

--- src/lib/bottom_bar.svelte
+++ src/lib/bottom_bar.svelte
@@ -28,40 +28,42 @@
 	}
 </script>
 
-<div class="w-full flex items-center flex-col px-4">
-	<form
-		class="m-2 mx-4 flex max-w-prose gap-2 shadow w-full align-middle bg-stone-50 items-center border-stone-300 border-2 p-1 rounded-lg focus-within:shadow-lg focus-within:border-stone-400 transition-all dark:bg-stone-800 dark:border-stone-700"
-	>
-		<textarea
-			tabindex="9999"
-			class="resize-none outline-none rounded bg-stone-50 py-1 px-2 text-stone-950 flex-grow dark:bg-stone-800 dark:text-stone-100 transition-all"
-			bind:value={prompt}
-			bind:this={textArea}
-			on:input={autoResize}
-			on:keydown={handleKeyDown}
-			rows="1"
-			placeholder="Ask me something about llamas..."
-		></textarea>
-		<button
-			class="bg-stone-200 text-stone-500 hover:text-stone-700 hover:cursor-pointer hover:bg-stone-300 m-1 p-1 transition-all rounded-xl w-8 h-8 dark:bg-stone-700 dark:text-stone-400 dark:hover:bg-stone-600 dark:hover:text-stone-300"
-			type="submit"
-			on:click={toggleChat}
+<div class="w-full flex items-center justify-around flex-col px-4">
+	<div class="max-w-prose w-full md:px-6">
+		<form
+			class="mb-2 max-w-prose flex gap-2 shadow w-full align-middle bg-stone-50 items-center border-stone-300 border-2 p-1 rounded-lg focus-within:shadow-lg focus-within:border-stone-400 transition-all dark:bg-stone-800 dark:border-stone-700"
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-6 h-6"
+			<textarea
+				tabindex="9999"
+				class="resize-none outline-none rounded bg-stone-50 py-1 px-2 text-stone-950 flex-grow dark:bg-stone-800 dark:text-stone-100 transition-all"
+				bind:value={prompt}
+				bind:this={textArea}
+				on:input={autoResize}
+				on:keydown={handleKeyDown}
+				rows="1"
+				placeholder="Ask me something about llamas..."
+			></textarea>
+			<button
+				class="bg-stone-200 text-stone-500 hover:text-stone-700 hover:cursor-pointer hover:bg-stone-300 m-1 p-1 transition-all rounded-xl w-8 h-8 dark:bg-stone-700 dark:text-stone-400 dark:hover:bg-stone-600 dark:hover:text-stone-300"
+				type="submit"
+				on:click={toggleChat}
 			>
-				<path
-					in:draw={{ duration: 500 }}
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-				/>
-			</svg>
-		</button>
-	</form>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						in:draw={{ duration: 500 }}
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+					/>
+				</svg>
+			</button>
+		</form>
+	</div>
 </div>
