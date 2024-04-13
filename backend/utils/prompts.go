a2bf4605e3a6262769d41a2949a03576bbe37be9
Nils Herzig <git@nilsherzig.com>
2024-04-13 14:49:50 +0200

    prompts: reverted back to my own prompts

--- backend/utils/prompts.go
+++ backend/utils/prompts.go
@@ -8,16 +8,11 @@ func ParsingErrorPrompt() string {
 
 func GenerateSystemMessage() string {
 	systemMessage := []string{}
-	systemMessage = append(systemMessage, "Strictly structure your answers in markdown for enhanced readability and organization.")
+	systemMessage = append(systemMessage, "1. Format your answer in markdown.")
+	systemMessage = append(systemMessage, "2. You have to use your tools to answer quesions.")
+	systemMessage = append(systemMessage, "3. You have to provide sources / links you've used to answer the question.")
+	systemMessage = append(systemMessage, "4. You may use tools more than once.")
+	systemMessage = append(systemMessage, "5. Answer in the same language as the quesion.")
 
-	systemMessage = append(systemMessage, "Mandatory: Employ all available tools to formulate well-informed and accurate responses. Guesswork is not permitted.")
-
-	// systemMessage = append(systemMessage, "First consult VectorDB for existing insights on ongoing topics before considering a new web search. This step is crucial for efficient and informed response generation.")
-
-	systemMessage = append(systemMessage, "Mandatory: All answers must include cited sources to ensure the information provided is traceable and credible.")
-
-	systemMessage = append(systemMessage, "It is imperative to reply in the same language as the query. This ensures clarity and relevance in communication.")
-
-	systemMessage = append(systemMessage, "For optimal search results, formulate queries concisely and use specific keywords related to the inquiry. Avoid vague terms to ensure the precision and relevance of the results.")
 	return strings.Join(systemMessage, " ")
 }
