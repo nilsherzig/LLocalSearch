de4cd308196c5cef31727c7bccfc12b2889ed469
Nils Herzig <git@nilsherzig.com>
2024-05-07 16:37:19 +0200

    some nginx buffering things

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		})
